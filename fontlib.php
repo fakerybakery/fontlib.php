9c3fd565f1be671cb30f3f42be89a30bbd9e997f
mrfakename <me@mrfake.name>
2023-04-17 13:04:50 -0700

    v1.0.0

--- fontlib.php
+++ fontlib.php
@@ -12,6 +12,7 @@
  * Not affiliated with Google Fonts
  *
  */
+ 
 $availablefonts = ["Anuphan", "Architects Daughter", "Caveat", "Courgette", "DM Sans", "Dancing Script", "Delicious Handrawn", "Gloria Hallelujah", "Gochi Hand", "Golos Text", "Hanken Grotesk", "Indie Flower", "Kalam", "Marck Script", "Nanum Pen Script", "Pacifico", "Permanent Marker", "Roboto", "Rock Salt", "Satisfy", "Schibsted Grotesk", "Schoolbell", "Sedgwick Ave Display", "Shadows Into Light", "Sriracha"];
 function generateFonts($usedFonts) {
     $url = 'https://fonts.googleapis.com/css2';
