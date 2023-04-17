080a784c50db863a3e9717787a81698f079626dc
mrfakename <me@mrfake.name>
2023-04-17 13:06:05 -0700

    Update fontlib.php

--- fontlib.php
+++ fontlib.php
@@ -10,7 +10,7 @@
  * Website: www.mrfake.name
  *
  * Not affiliated with Google Fonts
- *
+ * https://github.com/fakerybakery/fontlib.php
  */
  
 $availablefonts = ["Anuphan", "Architects Daughter", "Caveat", "Courgette", "DM Sans", "Dancing Script", "Delicious Handrawn", "Gloria Hallelujah", "Gochi Hand", "Golos Text", "Hanken Grotesk", "Indie Flower", "Kalam", "Marck Script", "Nanum Pen Script", "Pacifico", "Permanent Marker", "Roboto", "Rock Salt", "Satisfy", "Schibsted Grotesk", "Schoolbell", "Sedgwick Ave Display", "Shadows Into Light", "Sriracha"];
