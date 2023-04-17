bde0cfe7f3eaab61038740a719eacf9e839fb01f
mrfakename <me@mrfake.name>
2023-04-17 13:04:33 -0700

    Initial Commit\

---
+++ fontlib.php
@@ -0,0 +1,30 @@
+<?php
+/*
+ * fontlib, a mrfakename library
+ * fontlib is for PHP 7.4+
+ *
+ * Copyright © 2023 mrfakename.
+ * All rights reserved.
+ *
+ * Email: me [at] mrfake [dot] name
+ * Website: www.mrfake.name
+ *
+ * Not affiliated with Google Fonts
+ *
+ */
+$availablefonts = ["Anuphan", "Architects Daughter", "Caveat", "Courgette", "DM Sans", "Dancing Script", "Delicious Handrawn", "Gloria Hallelujah", "Gochi Hand", "Golos Text", "Hanken Grotesk", "Indie Flower", "Kalam", "Marck Script", "Nanum Pen Script", "Pacifico", "Permanent Marker", "Roboto", "Rock Salt", "Satisfy", "Schibsted Grotesk", "Schoolbell", "Sedgwick Ave Display", "Shadows Into Light", "Sriracha"];
+function generateFonts($usedFonts) {
+    $url = 'https://fonts.googleapis.com/css2';
+    $isFirst = true;
+    foreach ($usedFonts as $font) {
+        if ($isFirst) {
+            $url .= '?family=';
+            $isFirst = false;
+        } else {
+            $url .= '&family=';
+        }
+        $url .= urlencode($font);
+    }
+    $url .= '&display=swap';
+    return $url;
+}
